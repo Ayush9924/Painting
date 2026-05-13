e8977d97c9971784954a390f35ed7a67929b2cec
Ayush <karanjio2001@gmail.com>
2026-05-14 00:30:59 +0530

    new line

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ ctx.fillRect(100, 70, 200, 150);
 ctx.strokeStyle = 'rgba(152, 218, 152, 0.8)';
 ctx.lineWidth = 4;
 ctx.strokeRect(100, 70, 200, 150);
+
+ctx.beginPath();
+ctx.moveTo(100, 70);
+ctx.lineTo(300, 220);
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 2;
+ctx.stroke(); //use to draw the line
+
